3d8b1745a4c50ee627b5f6fdd73b999018ba6d7e
Soumik <soumik.m16@iiits.in>
2017-08-07 21:48:00 +0530

    Add files via upload

---
+++ mywebsite/music/views.py
@@ -0,0 +1,23 @@
+from django.http import Http404
+from django.http import HttpResponse
+from .models import Album,Song
+from django.shortcuts import render,get_object_or_404
+from django.template import loader
+
+
+def index(request) :
+
+    all_albums=Album.objects.all()
+
+
+    return render(request,'music/index.html',{'all_albums': all_albums})
+
+
+def detail(request, album_id):
+
+    album=get_object_or_404(Album,pk=album_id)
+
+    return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request,album_id):
+    album=get_object_or_404(Album,pk=album_id)
